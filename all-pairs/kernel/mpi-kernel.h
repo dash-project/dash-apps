ded530961805c66f9cf0e8592dfdcabd111988aa
Felix Moessbauer <moessbauer@cip.ifi.lmu.de>
2016-08-07 20:06:12 +0200

    use inheritance for mpi kernels

---
+++ all-pairs/kernel/mpi-kernel.h
@@ -0,0 +1,39 @@
+#ifndef MPI_KERNEL_H
+#define MPI_KERNEL_H
+
+#include <mpi.h>
+#include "all-pairs-kernel.h"
+
+class MPIKernel : public AllPairsKernel {
+
+protected:
+int      local_slots;
+int    * send_data;
+int    * recv_data;
+size_t   sr_size;
+int      repeat = 0;
+
+MPIKernel(int internal_repeats = 1, std::string name = "MPI")
+	: AllPairsKernel(internal_repeats, name)
+	{}
+
+~MPIKernel(){
+	dash::barrier();
+	MPI_Free_mem(send_data);
+	MPI_Free_mem(recv_data);
+}
+
+void init(int repeats){
+  sr_size = sizeof(int) * repeats * int_repeats;
+  MPI_Alloc_mem(sr_size, MPI_INFO_NULL, &send_data);
+  MPI_Alloc_mem(sr_size, MPI_INFO_NULL, &recv_data);
+
+	MPI_Barrier(MPI_COMM_WORLD);	
+}
+
+void reset(){
+  repeat = 0;
+}
+
+}
+#endif // MPI_KERNEL_H
--- all-pairs/kernel/rma-kernel.h
+++ all-pairs/kernel/rma-kernel.h
@@ -3,28 +3,22 @@
 
 #include <string>
 #include <mpi.h>
-#include "all-pairs-kernel.h"
+#include "mpi-kernel.h"
 
 /**
  * AllPairs Kernel concept
  */
-class RMAKernel : public AllPairsKernel {
+class RMAKernel : public MPIKernel {
 
 protected:
 
-int      local_slots;
-int    * send_data;
-int    * recv_data;
-size_t   sr_size;
-int      repeat = 0;
 MPI_Win  window_send;
 MPI_Win  window_recv;
-
 
 public:
 
 RMAKernel(int internal_repeats = 1, std::string name = "RMA")
-  : AllPairsKernel(internal_repeats, name)
+  : MPIKernel(internal_repeats, name)
   {}
 
 ~RMAKernel(){
@@ -33,16 +27,10 @@ RMAKernel(int internal_repeats = 1, std::string name = "RMA")
     MPI_Win_unlock_all(window_send);  
     MPI_Win_free(&window_recv);
     MPI_Win_free(&window_send);
-
-    MPI_Free_mem(send_data);
-    MPI_Free_mem(recv_data);
 }
 
 void init(int repeats){
-  sr_size = sizeof(int) * repeats * int_repeats;
-  MPI_Alloc_mem(sr_size, MPI_INFO_NULL, &send_data);
-  MPI_Alloc_mem(sr_size, MPI_INFO_NULL, &recv_data);
-
+		MPIKernel::init(repeats);
     MPI_Win_create(send_data,
                   sr_size,
                   sizeof(int),
@@ -62,18 +50,6 @@ void init(int repeats){
     MPI_Win_lock_all(0, window_recv);
     MPI_Barrier(MPI_COMM_WORLD);
 }
-
-void reset(){
-  repeat = 0;
 }
-
-/**
- Perform repeated measures on given data point
- */
-void run(int send, int recv){}
-
-};
-
-
 
 #endif // RMA_KERNEL_H 
