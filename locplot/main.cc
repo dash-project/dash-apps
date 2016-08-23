aa2e0354a2381d8fccab9024a530ee39fec5bb55
Tobias Fuchs <fuchst@nm.ifi.lmu.de>
2016-08-23 22:30:51 +0200

    Implementing locality hierarchy visualizer

--- locplot/main.cc
+++ locplot/main.cc
@@ -3,17 +3,53 @@
 
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
 
+using json = nlohmann::json;
+using std::cout;
+using std::cerr;
+using std::endl;
 
+
+std::string render_domain(
+  const json & elem,
+  int          parent_w,
+  int          parent_h)
+{
+  std::ostringstream os;
+
+  int w = parent_w;
+  int h = parent_h;
+
+  int x = 5;
+  int y = 5;
+
+  os << "<rect x=\"" << x << "\" y=\"" << y << "\" "
+     << "height=\"" << h << "\" width=\"" << w << "\" >"
+     << endl;
+
+  for (auto it = elem.begin(); it != elem.end(); ++it) {
+    if (it->is_string()) {
+      os << *it << endl;
+    }
+    if (it->is_object()) {
+      if (it.key() == "NODE") {
+        os << "NODE: " << it.value() << endl;
+      } else {
+        os << render_domain(*it, w, h);
+      }
+    }
+  }
+
+  os << endl;
+  os << "</rect>" << std::endl;
+
+  return os.str();
+}
 
 int main(int argc, char * argv[])
 {
-  using json = nlohmann::json;
-  using std::cout;
-  using std::cerr;
-  using std::endl;
-
   if (argc < 2) {
     cerr << "no filename specified" << endl;
     return EXIT_FAILURE;
@@ -33,6 +69,16 @@ int main(int argc, char * argv[])
   loc_json_ifs.close();
 
   auto loc_hierarchy = json::parse(loc_json_str);
+
+  std::ostringstream os;
+  os << "<svg xmlns=\"http://www.w3.org/2000/svg\"";
+  os << " xmlns:xlink=\"http://www.w3.org/1999/xlink\">\n";
+
+  for (auto & elem : loc_hierarchy) {
+    os << render_domain(elem, 100, 100);
+  }
+
+  cout << os.str() << endl;
 
   return EXIT_SUCCESS;
 }
