ed89afcfe23f52798866fc8e590b5596e337b20d
Clemens Manert <Clemens@Manert.de>
2016-10-09 04:22:23 +0200

    Added Norm.

--- NPB-3.3.1/CG/vector.hh
+++ NPB-3.3.1/CG/vector.hh
@@ -6,11 +6,29 @@
 template<class TValue>
 class Vector {
 
-  dash::Array<TValue> _internal;
-
+  dash::Array<TValue> & _internal;
+  
 public:
-  Vector(int size) : _internal(size) {
+  Vector(dash::Array<TValue> & internal) : _internal(internal) { }
+
+  TValue Norm() {
+
+    dash::Array<TValue> l_results(dash::size());
+    l_results.local[0] =
+      std::accumulate(
+		       _internal.lbegin(),
+		       _internal.lend(),
+		       0,
+		       [](TValue x1, TValue x2){
+			 return x1 + x2*x2;
+		       });
+
+
+    return  dash::accumulate(l_results.begin(),
+			     l_results.end(),
+			     0, dash::plus<TValue>());
   }
+
 };
 
 #endif // VECTOR_HH_
