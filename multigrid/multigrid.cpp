8a6ce4f3438e67752c911a3d6cf7b2ab3246a4c4
dhinf <denis.huenich@tu-dresden.de>
2017-08-03 15:36:03 +0200

    changed to fixed Halo boundaries in 2D multigrid, added leading zero to filename numbers and fixed some bugs

--- multigrid/multigrid.cpp
+++ multigrid/multigrid.cpp
@@ -1,5 +1,6 @@
 #include <unistd.h>
 #include <iostream>
+#include <sstream>
 #include <cstddef>
 #include <iomanip>
 #include <cassert>
@@ -24,12 +25,14 @@ using std::endl;
 using std::setw;
 using std::vector;
 
-using TeamSpecT = dash::TeamSpec<2>;
-using MatrixT = dash::NArray<double,2>;
-using StencilT = dash::experimental::Stencil<2>;
-using StencilSpecT = dash::experimental::StencilSpec<2,4>;
+using TeamT              = dash::Team;
+using TeamSpecT          = dash::TeamSpec<2>;
+using MatrixT            = dash::NArray<double,2>;
+using StencilT           = dash::experimental::Stencil<2>;
+using StencilSpecT       = dash::experimental::StencilSpec<2,4>;
 using HaloMatrixWrapperT = dash::experimental::HaloMatrixWrapper<MatrixT, StencilSpecT>;
-
+using CycleSpecT         = dash::experimental::CycleSpec<2>;
+using Cycle              = dash::experimental::Cycle;
 
 const StencilSpecT stencil_spec({
     StencilT(-1, 0), StencilT( 1, 0),
@@ -41,10 +44,10 @@ struct Level {
     MatrixT grid;
     HaloMatrixWrapperT halo;
 
-    Level( size_t w, size_t h, dash::Team& team, TeamSpecT teamspec ) :
-        grid( dash::SizeSpec<2>( h+2, w+2 ),
+    Level( size_t w, size_t h, TeamT& team, TeamSpecT& teamspec ) :
+        grid( dash::SizeSpec<2>( h, w ),
               dash::DistributionSpec<2>( dash::BLOCKED, dash::BLOCKED ), team, teamspec ),
-        halo( grid, stencil_spec ) {}
+        halo( grid, stencil_spec, CycleSpecT(Cycle::FIXED, Cycle::FIXED )) {}
     Level() = delete;
 
     ~Level() {}
@@ -59,6 +62,7 @@ void topng( const MatrixT& grid, double minvalue= 0.0, double maxvalue= 100.0 ) 
     uint32_t f= 1;
     size_t w= grid.extent(1);
     size_t h= grid.extent(0);
+
 
     if ( w < minsize || h < minsize ) {
 
@@ -86,8 +90,9 @@ void topng( const MatrixT& grid, double minvalue= 0.0, double maxvalue= 100.0 ) 
             }
         }
     }
-
-    image.write( std::string(filename + std::to_string(filenumber++) + std::string(".png")).c_str() );
+    std::stringstream ss;
+    ss << filename << std::setfill('0') << setw(2) << filenumber++ << ".png";
+    image.write( ss.str().c_str() );
 }
 #endif /* WITHPNGOUTPUT */
 
@@ -148,127 +153,64 @@ void initgrid( MatrixT& grid ) {
 
     cout << "assign values to finest level" << endl;
 
-    for( size_t i = 1; i < h-1; ++i ) {
-    for( size_t k = 1; k < w-1; ++k ) {
+    for( size_t i = 0; i < h; ++i )
+      for( size_t k = 0; k < w; ++k )
         grid[i][k] = 7.0;
-    }
-    }
 
-    for( size_t i = h/5; i < h*3/4; ++i ) {
-    for( size_t k = w*1/5; k < w*1/3; ++k ) {
+    for( size_t i = h/5; i < h*3/4; ++i )
+      for( size_t k = w*1/5; k < w*1/3; ++k )
         grid[i][k] = 3.0;
-    }
-    }
 }
 
 
-void setboundary( MatrixT& grid ) {
-
-    if ( 0 != dash::myid() ) return;
+void setboundary( Level& level ) {
 
     /* do initialization only from unit 0 with global accesses.
     This is not the fastest way and a parallel routine would not be too
     complicated but it should demonstrate the possibility to trade
     convenience against performance. */
 
-    size_t w= grid.extent(1);
-    size_t h= grid.extent(0);
 
-    /* set entire boundary to a low value */
-    for ( size_t x= 0; x < w; x++ ) {
-        grid[0  ][x]= 5.0;
-        grid[h-1][x]= 5.0;
-    }
-    for ( size_t y= 0; y < h; y++ ) {
-        grid[y][0  ]= 5.0;
-        grid[y][w-1]= 5.0;
-    }
+    level.halo.setFixedHalos([&level](const std::array<dash::default_index_t,2>& coords) {
+      size_t w= level.grid.extent(1);
+      size_t h= level.grid.extent(0);
 
-    /* set parts of boundary to a high value */
-    for ( size_t x= 0; x < w/3; x++ ) {
-        grid[0  ][x]= 10.0;
-    }
-    for ( size_t y= 0; y < h*3/5; y++ ) {
-        grid[y][0  ]= 10.0;
-    }
+      if(coords[0] == -1 && coords[1] < w/3 ||
+         coords[0] < h*3/5 && coords[1] == -1) {
+        return 10.0;
+      }
 
-    /* set other parts of boundary to a very low value */
-    for ( size_t y= h*1/5; y < h*2/5; y++ ) {
-        grid[y][w-1]= 0.0;
-    }
-    for ( size_t y= h*3/5; y < h*4/5; y++ ) {
-        grid[y][w-1]= 0.0;
-    }
+      if(coords[0] >= h*1/5 && coords[0] < h*2/5 && coords[1] == w  ||
+         coords[0] >= h*3/5 && coords[0] < h*4/5 && coords[1] == w ) {
+        return 0.0;
+      }
+
+
+      return 5.0;
+    });
 }
-
-
-void scaledownboundary( const MatrixT& fine, MatrixT& coarse ) {
-
-    assert( (coarse.extent(1)-2)*2 +2 == fine.extent(1) );
-    assert( (coarse.extent(0)-2)*2 +2 == fine.extent(0) );
-
-    size_t wc= coarse.local.extent(1);
-    size_t hc= coarse.local.extent(0);
-    size_t wf= fine.local.extent(1);
-    size_t hf= fine.local.extent(0);
-    std::array< long int, 2 > cornerc= coarse.pattern().global( {0,0} );
-
-    size_t startx= ( 0 == cornerc[1] ) ? 1 : 0;
-    if ( 0 == cornerc[0] ) {
-
-        /* if boundary in front */
-        for ( size_t x= 0; x < wc-1; x++ ) {
-            coarse.local[0][startx+x]= 0.5 * (double) ( fine.local[0][startx+2*x+0] + fine.local[0][startx+2*x+1] );
-        }
-
-    } else {
-
-        /* else boundary in the end -- this is only true for the restructed case with 4 units */
-        for ( size_t x= 0; x < wc-1; x++ ) {
-            coarse.local[hc-1][startx+x]= 0.5 * ( fine.local[hf-1][startx+2*x+0] + fine.local[hf-1][startx+2*x+1] );
-        }
-    }
-
-    size_t starty= ( 0 == cornerc[0] ) ? 1 : 0;
-    if ( 0 == cornerc[1] ) {
-
-        /* if boundary in front */
-        for ( size_t y= 0; y < hc-1; y++ ) {
-            coarse.local[starty+y][0]= 0.5 * ( fine.local[starty+2*y+0][0] + fine.local[starty+2*y+1][0] );
-        }
-
-    } else {
-
-        /* else boundary in the end -- this is only true for the restructed case with 4 units */
-        for ( size_t y= 0; y < hc-1; y++ ) {
-            coarse.local[starty+y][wc-1]= 0.5 * ( fine.local[starty+2*y+0][wf-1] + fine.local[starty+2*y+1][wf-1] );
-        }
-    }
-}
-
 
 void scaledown( Level& fine, Level& coarse ) {
 
-    assert( (coarse.grid.extent(1)-2)*2 +2 == fine.grid.extent(1) );
-    assert( (coarse.grid.extent(0)-2)*2 +2 == fine.grid.extent(0) );
+    assert( (coarse.grid.extent(1))*2 == fine.grid.extent(1) );
+    assert( (coarse.grid.extent(0))*2 == fine.grid.extent(0) );
 
     fine.halo.updateHalosAsync();
 
-    const std::array< long unsigned int, 2 > extentc= coarse.grid.pattern().local_extents();
-    const std::array< long signed int, 2 >   cornerc= coarse.grid.pattern().global( {0,0} );
-    const std::array< long unsigned int, 2 > extentf= fine.grid.pattern().local_extents();
+    const auto& extentc = coarse.grid.pattern().local_extents();
+    const auto cornerc = coarse.grid.pattern().global( {0,0} );
 
-    size_t startx= ( 0 == cornerc[1] % 2 ) ? 1 : 0;
-    size_t starty= ( 0 == cornerc[0] % 2 ) ? 1 : 0;
+    size_t startx= ( 0 == cornerc[1] % 2 ) ? 0 : 1;
+    size_t starty= ( 0 == cornerc[0] % 2 ) ? 0 : 1;
 
     for ( size_t y= 0; y < extentc[0]-starty ; y++ ) {
         for ( size_t x= 0; x < extentc[1]-startx ; x++ ) {
 
-            coarse.grid.local[starty+y][startx+x]= 0.25 * (
-                fine.grid.local[starty+2*y  ][startx+2*x  ] +
-                fine.grid.local[starty+2*y  ][startx+2*x+1] +
-                fine.grid.local[starty+2*y+1][startx+2*x  ] +
-                fine.grid.local[starty+2*y+1][startx+2*x+1] );
+            coarse.grid.local(starty+y, startx+x)= 0.25 * (
+                fine.grid.local(starty+2*y  , startx+2*x  ) +
+                fine.grid.local(starty+2*y  , startx+2*x+1) +
+                fine.grid.local(starty+2*y+1, startx+2*x  ) +
+                fine.grid.local(starty+2*y+1, startx+2*x+1) );
         }
     }
 
@@ -279,7 +221,7 @@ void scaledown( Level& fine, Level& coarse ) {
 
         // cout << "first row globally is boundary condition, nothing to do here" << endl;
 
-    } else if ( 0 == cornerc[0] % 2 ) {
+    } else if ( !cornerc[0] ) {
 
         cout << dash::myid() << ": first row locally is even, thus need to do scale down with halo row" << endl;
 
@@ -290,7 +232,7 @@ void scaledown( Level& fine, Level& coarse ) {
 
         // cout << "last row globally is boundary condition, nothing to do here" << endl;
 
-    } else if ( 0 == ( cornerc[0] + extentc[0] ) % 2 ) {
+    } else if ( 1 == ( cornerc[0] + extentc[0] ) % 2 ) {
 
         cout << dash::myid() << ": last row locally is odd (following first row in next block is even), thus need to do scale down with halo row" << endl;
     } // else nothing to do
@@ -299,7 +241,7 @@ void scaledown( Level& fine, Level& coarse ) {
 
         // cout << "first column globally is boundary condition, nothing to do here" << endl;
 
-    } else if ( 0 == cornerc[1] % 2 ) {
+    } else if ( !cornerc[1] ) {
 
         cout << dash::myid() << ": first column locally is even, thus need to do scale down with halo column" << endl;
 
@@ -334,23 +276,23 @@ void scaledown( Level& fine, Level& coarse ) {
 
 void scaleup( Level& coarse, Level& fine ) {
 
-    assert( (coarse.grid.extent(1)-2)*2 +2 == fine.grid.extent(1) );
-    assert( (coarse.grid.extent(0)-2)*2 +2 == fine.grid.extent(0) );
+    assert( (coarse.grid.extent(1))*2  == fine.grid.extent(1) );
+    assert( (coarse.grid.extent(0))*2  == fine.grid.extent(0) );
 
     size_t w= coarse.grid.local.extent(1);
     size_t h= coarse.grid.local.extent(0);
-    std::array< long int, 2 > cornerc= coarse.grid.pattern().global( {0,0} );
-    size_t startx= ( 0 == cornerc[1] ) ? 1 : 0;
-    size_t starty= ( 0 == cornerc[0] ) ? 1 : 0;
+    const auto cornerc= coarse.grid.pattern().global( {0,0} );
+    size_t startx= ( 0 == cornerc[1] % 2) ? 0 : 1;
+    size_t starty= ( 0 == cornerc[0] % 2) ? 0 : 1;
 
-    for ( size_t y= 0; y < h-1 ; y++ ) {
-        for ( size_t x= 0; x < w-1 ; x++ ) {
+    for ( size_t y= 0; y < h ; y++ ) {
+        for ( size_t x= 0; x < w ; x++ ) {
 
-            double t= coarse.grid.local[starty+y][startx+x];
-            fine.grid.local[starty+2*y  ][startx+2*x  ]= t;
-            fine.grid.local[starty+2*y  ][startx+2*x+1]= t;
-            fine.grid.local[starty+2*y+1][startx+2*x  ]= t;
-            fine.grid.local[starty+2*y+1][startx+2*x+1]= t;
+            double t= coarse.grid.local(starty+y,startx+x);
+            fine.grid.local(starty+2*y  , startx+2*x  )= t;
+            fine.grid.local(starty+2*y  , startx+2*x+1)= t;
+            fine.grid.local(starty+2*y+1, startx+2*x  )= t;
+            fine.grid.local(starty+2*y+1, startx+2*x+1)= t;
         }
     }
 
@@ -360,13 +302,13 @@ void scaleup( Level& coarse, Level& fine ) {
 
 double smoothen( Level& level ) {
 
-    double res= 0.0;
+    double res = 0.0;
 
     /// relaxation coeff.
-    const double c= 1.0;
+    const double c = 1.0;
 
     /// begin pointer of local block, needed because halo border iterator is read-only
-    auto gridlocalbegin= level.grid.lbegin();
+    auto gridlocalbegin = level.grid.lbegin();
 
     // async halo update
     level.halo.updateHalosAsync();
@@ -381,20 +323,19 @@ double smoothen( Level& level ) {
     the border update -- or there is an outside border -- then the first column or row
     contains the boundary values. */
 
-    for ( size_t y= 1; y < lh-1; y++ ) {
+    for ( size_t y = 1; y < lh - 1; y++ ) {
 
-        const size_t x= 1;
-        double* p_here= &gridlocalbegin[ y*lw+x ];
-        double* p_east= &gridlocalbegin[ y*lw+x+1 ];
-        double* p_west= &gridlocalbegin[ y*lw+x-1 ];
-        double* p_north= &gridlocalbegin[ (y-1)*lw+x ];
-        double* p_south= &gridlocalbegin[ (y+1)*lw+x ];
+        double* p_here= &gridlocalbegin[ y*lw + 1 ];
+        double* p_east= &gridlocalbegin[ y*lw+1 + 1 ];
+        double* p_west= &gridlocalbegin[ y*lw-1 + 1 ];
+        double* p_north= &gridlocalbegin[ (y-1)*lw + 1 ];
+        double* p_south= &gridlocalbegin[ (y+1)*lw + 1];
 
-        for ( size_t x= 1; x < lw-1; x++ ) {
+        for ( size_t x = 1; x < lw - 1; x++ ) {
 
             double dtheta = ( *p_east + *p_west + *p_north + *p_south ) / 4.0 - *p_here ;
             *p_here += c * dtheta;
-            res= std::max( res, std::fabs( dtheta ) );
+            res = std::max( res, std::fabs( dtheta ) );
             p_here++;
             p_east++;
             p_west++;
@@ -409,15 +350,14 @@ double smoothen( Level& level ) {
     auto bend = level.halo.bend();
     // update border area
     for( auto it = level.halo.bbegin(); it != bend; ++it ) {
-
         double dtheta = ( it.valueAt(2) + it.valueAt(3) +
                           it.valueAt(0) + it.valueAt(1) ) / 4.0 - *it;
         gridlocalbegin[ it.lpos() ] += c * dtheta;
-        res= std::max( res, std::fabs( dtheta ) );
+        res = std::max( res, std::fabs( dtheta ) );
     }
 
     dash::Array<double> residuals( dash::Team::All().size(), dash::BLOCKED );
-    residuals.local[0]= res;
+    residuals.local[0] = res;
 
     residuals.barrier();
 
@@ -444,7 +384,7 @@ void v_cycle( vector<Level*>& levels, uint32_t inneriterations= 1 ) {
 
     for ( auto i= 1; i < levels.size(); i++ ) {
 
-        double res= smoothen( *levels[i-1] );
+        double res = smoothen( *levels[i-1] );
 
         writeToPNG( levels[i-1]->grid );
 
@@ -457,8 +397,6 @@ void v_cycle( vector<Level*>& levels, uint32_t inneriterations= 1 ) {
         }
 
         writeToPNG( levels[i]->grid );
-
-        //exit(1);
     }
 
     /* do a fixed number of smoothing steps until the residual on the coarsest grid is
@@ -496,7 +434,7 @@ int main( int argc, char* argv[] ) {
 
     dash::init(&argc, &argv);
 
-    TeamSpecT teamspec( dash::Team::All().size(), 1 );
+    TeamSpecT teamspec( TeamT::All().size(), 1 );
     teamspec.balance_extents();
 
     constexpr uint32_t howmanylevels= 10;
@@ -507,7 +445,7 @@ int main( int argc, char* argv[] ) {
     for ( auto l= 0; l < howmanylevels-3; l++ ) {
 
         if ( 0 == dash::myid() ) {
-            cout << "level " << l << " is " << (1<<(howmanylevels-l))+2 << "x" << (1<<(howmanylevels-l))+2 << endl;
+            cout << "level " << l << " is " << (1<<(howmanylevels-l)) << "x" << (1<<(howmanylevels-l)) << endl;
         }
 
         levels.push_back( new Level( (1<<(howmanylevels-l)), (1<<(howmanylevels-l)),
@@ -520,13 +458,10 @@ int main( int argc, char* argv[] ) {
         dash::barrier();
 
 
-        if ( 0 == l ) {
+        if ( 0 == l )
             sanitycheck( levels[0]->grid );
-            setboundary( levels[0]->grid );
-            continue;
-        }
 
-        scaledownboundary( levels[l-1]->grid, levels[l]->grid );
+        setboundary( *levels[l] );
     }
 
     dash::barrier();
@@ -536,7 +471,6 @@ int main( int argc, char* argv[] ) {
     initgrid( levels[0]->grid );
 
     dash::barrier();
-
     v_cycle( levels, 10 );
 
     double res= smoothen( *levels.front() );
