cfc0e419223a946f88b092b1c7dc97987ace584c
Andreas Knüpfer <knue@posteo.de>
2017-08-23 21:07:16 +0200

    use merge sort

--- multigrid/combine_image_csvs.sh
+++ multigrid/combine_image_csvs.sh
@@ -3,6 +3,7 @@
 for i in image_unit0.csv.*; do
     NUM=`echo $i|sed -e 's/image_unit0.csv.//'`
     echo -n $NUM" "
-    cat "image_unit"*".csv."$NUM | sort > "image.csv."$NUM
+    #cat "image_unit"*".csv."$NUM | sort > "image.csv."$NUM
+    sort -m "image_unit"*".csv."$NUM > "image.csv."$NUM
     rm "image_unit"*".csv."$NUM
 done
