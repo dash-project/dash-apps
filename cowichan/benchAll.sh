110ea102b8a5bf49e361226e65bc882f62864008
Benedikt Lehmann <lehmann.benedikt1@gmail.com>
2017-08-25 14:11:45 +0200

    adjusted parameters for bench

--- cowichan/benchAll.sh
+++ cowichan/benchAll.sh
@@ -3,17 +3,17 @@
 
 probIxStart=0
 probIxEnd=5
-jobsStart=24
+jobsStart=1
 jobsEnd=24
-numberOfIterations=1
+numberOfIterations=5
 nRowsCols=(100 400 4000 40000)
 thresh=(10 25 50 75 100)
 winnowNelts=(100 400 4000 40000)
 
 
-for (( nRowsColsIX=0 ; nRowsColsIX<1 ; ++nRowsColsIX )); do
-  for (( threshIX=0 ; threshIX<1 ; ++threshIX )); do
-    for (( winnowNeltsIX=0 ; winnowNeltsIX<1 ; ++winnowNeltsIX )); do
+for (( nRowsColsIX=0 ; nRowsColsIX<4 ; ++nRowsColsIX )); do
+  for (( threshIX=0 ; threshIX<5 ; ++threshIX )); do
+    for (( winnowNeltsIX=0 ; winnowNeltsIX<4 ; ++winnowNeltsIX )); do
       ./benchDash.sh $probIxStart $probIxEnd $jobsStart $jobsEnd $numberOfIterations ${nRowsCols[$nRowsColsIX]} ${thresh[$threshIX]} ${winnowNelts[$winnowNeltsIX]}
       ./benchTBB.sh $probIxStart $probIxEnd $jobsStart $jobsEnd $numberOfIterations ${nRowsCols[$nRowsColsIX]} ${thresh[$threshIX]} ${winnowNelts[$winnowNeltsIX]}
       ./benchChapel.sh $probIxStart $probIxEnd $jobsStart $jobsEnd $numberOfIterations ${nRowsCols[$nRowsColsIX]} ${thresh[$threshIX]} ${winnowNelts[$winnowNeltsIX]}
