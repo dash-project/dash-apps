c2f922edb4b53029274a153e149d06153d5709b0
Andreas Knüpfer <knue@posteo.de>
2017-08-30 14:12:47 +0200

    optional label for the new files

--- multigrid/combine_csvs.sh
+++ multigrid/combine_csvs.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+# if present, the first command line argument is used as a label. It should not contain spaces.
+
 for i in image_unit0.csv.*; do
     NUM=`echo $i|sed -e 's/image_unit0.csv.//'`
     echo -n $NUM" "
     #cat "image_unit"*".csv."$NUM | sort > "image.csv."$NUM
-    sort -m "image_unit"*".csv."$NUM > "image.csv."$NUM
+    sort -m "image_unit"*".csv."$NUM > "image_"$1".csv."$NUM
     rm "image_unit"*".csv."$NUM
 done
 
-cat trace0*.csv >trace_.csv
+cat trace0*.csv >"trace_"$1".csv"
 rm -Rf trace0*.csv
