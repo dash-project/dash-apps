9b999b43cdd17211a4bec118d0c45ff649e771ad
Daniel Diefenthaler <diefenthaler@cip.ifi.lmu.de>
2017-09-26 16:50:04 +0200

    display result below params

--- pattern-visualizer/htdocs/pattern.php
+++ pattern-visualizer/htdocs/pattern.php
@@ -1,11 +1,25 @@
 <?php
-$pattern = " -s ".$_GET['pattern'];
-$size    = " -n ".$_GET['size_0']." ".$_GET['size_1'];
-$units   = " -u ".$_GET['units_0']." ".$_GET['units_1'];
-$tile    = " -t ".$_GET['tile_0']." ".$_GET['tile_1'];
-$blocked = (strcmp($_GET['blocked_display'],"true") == 0)?" -b":"";
-$balance = (strcmp($_GET['balance_extents'],"true") == 0)?" -e":"";
+
+function getByName($name, $group) {
+  foreach($group->content as $value) {
+    if($name == $value->name) {
+      return $value;
+    }
+  }
+  return NULL;
+}
+
+$params = json_decode(file_get_contents("php://input"));
+$dimGroup = getByName("dim_group",$params);
+
+$pattern = " -s ".getByName("pattern",$params)->value->name;
+$size    = " -n ".getByName("size",$dimGroup)->value[0]." ".getByName("size",$dimGroup)->value[1];
+$units   = " -u ".getByName("units",$dimGroup)->value[0]." ".getByName("units",$dimGroup)->value[1];
+$tile    = " -t ".getByName("tile",$dimGroup)->value[0]." ".getByName("tile",$dimGroup)->value[1];
+$blocked = (strcmp(getByName("blocked_display",$params)->value,"true") == 0)?" -b":"";
+$balance = (strcmp(getByName("balance_extents",$params)->value,"true") == 0)?" -e":"";
 
 header("Content-Type: image/svg+xml");
+//echo "./pattern-visualizer -p".$blocked.$pattern.$balance.$units.$size.$tile;
 passthru("./pattern-visualizer -p".$blocked.$pattern.$balance.$units.$size.$tile);
 ?>
---
+++ pattern-visualizer/htdocs/submit.js
@@ -0,0 +1,34 @@
+function submitParams(e) {
+  e.preventDefault();
+  sendPOST();
+}
+
+function sendPOST() {
+  var req = new XMLHttpRequest();
+  req.open("POST","pattern.php");
+  req.addEventListener("load", function(e) {
+    var svgText = e.target.responseText;
+    var parser = new DOMParser();
+    var result = parser.parseFromString(svgText,"image/svg+xml");
+    console.log(result);
+    displayResult(result.activeElement);
+  });
+  req.addEventListener("error", function(e) {
+    var errorMsg = "Error loading visualized pattern, try again";
+    console.warn(e);
+    console.error(errorMsg);
+    displayResult(document.createTextNode(errorMsg));
+  });
+  req.send(JSON.stringify(getParams()));
+}
+
+function displayResult(result) {
+  var cont = document.getElementById("result");
+  // clear previous result
+  while(cont.firstChild) {
+    cont.removeChild(cont.firstChild);
+  }
+
+  cont.appendChild(result);
+}
+
