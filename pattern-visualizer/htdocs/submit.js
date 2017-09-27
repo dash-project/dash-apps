910bab82d782b42f4dfdbb8c4b2fefaeae2ef711
Daniel Diefenthaler <diefenthaler@cip.ifi.lmu.de>
2017-09-27 13:18:49 +0200

    Added a export to get the svg, broken in multiple browsers

--- pattern-visualizer/htdocs/submit.js
+++ pattern-visualizer/htdocs/submit.js
@@ -10,14 +10,20 @@ function sendPOST() {
     var svgText = e.target.responseText;
     var parser = new DOMParser();
     var result = parser.parseFromString(svgText,"image/svg+xml");
+    var exportLink = document.createElement("a");
+    exportLink.setAttribute("href","data:image/svg+xml;base64,"+window.btoa(svgText));
+    exportLink.setAttribute("target","_blank")
+    exportLink.appendChild(document.createTextNode("Export as svg"));
+    var imageDiv = document.createElement("div");
+    imageDiv.appendChild(result.documentElement);
     console.log(result);
-    displayResult(result.documentElement);
+    displayResult([exportLink,imageDiv]);
   });
   req.addEventListener("error", function(e) {
     var errorMsg = "Error loading visualized pattern, try again";
     console.warn(e);
     console.error(errorMsg);
-    displayResult(document.createTextNode(errorMsg));
+    displayResult([document.createTextNode(errorMsg)]);
   });
   req.send(JSON.stringify(getParams()));
 }
@@ -29,6 +35,8 @@ function displayResult(result) {
     cont.removeChild(cont.firstChild);
   }
 
-  cont.appendChild(result);
+  for(var i=0; i < result.length; i++) {
+    cont.appendChild(result[i]);
+  }
 }
 
