ccd8f8cfac9bdce34ccd7ea5bab70a38aaeffc83
Felix Moessbauer <felix.moessbauer@campus.lmu.de>
2017-10-31 14:11:45 +0100

    updated jobfiles for supermuc and cori

--- hpccg/jobfiles/supermuc/schedule-job.sh
+++ hpccg/jobfiles/supermuc/schedule-job.sh
@@ -2,15 +2,15 @@
 
 [[ !  -z  "$1"  ]] || { echo "usage: $0 <Jobtemplate>"; exit 1; }
 TPL="$1"
-UNITS="1 2 4 8 16 32 64 128 256"
-PPN="32"
+UNITS="1 2 4 8 16 28 56 112 224"
+PPN="28"
 
 for NUNITS in $UNITS; do
   JOBOUT="$TPL.$NUNITS"
   NODES=$((($NUNITS-1) / ($PPN+1) +1))
   sed "s/<procs>/$NUNITS/g" $TPL > $JOBOUT
   sed -i "s/<nodes>/$NODES/g" $JOBOUT
-  sbatch $JOBOUT
+  llsubmit $JOBOUT
   rm $JOBOUT
 done
 
