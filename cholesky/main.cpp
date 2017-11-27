e6f9fe50b0fd3a55afd7548b6aee11cf9fa49b8b
Joseph Schuchart <joseph.schuchart@hlrs.de>
2017-11-27 13:20:51 +0900

    Fix computation and add single-unit version

--- cholesky/main.cpp
+++ cholesky/main.cpp
@@ -5,12 +5,62 @@
 #include <iostream>
 #include <stdlib.h>
 
-using TiledMatrix = dash::Matrix<double, 2, dash::default_index_t, typename dash::ShiftTilePattern<2>>;
-static const size_t block_size = 5;
+using value_t = double;
+using PatternT = typename dash::ShiftTilePattern<2>;
+//using PatternT = typename dash::TilePattern<2>;
+using TiledMatrix = dash::Matrix<value_t, 2, dash::default_index_t, PatternT>;
+static const size_t N = 20;
+#define BLOCKS_PER_UNIT 5
+#define NUM_BLOCKS      10
 
+//#define DEBUG
+#define CHECK_RESULT
+
+extern "C" {
+void dgemm_ (const char *transa, const char *transb, int *l, int *n, int *m, double *alpha,
+             const void *a, int *lda, void *b, int *ldb, double *beta, void *c, int *ldc);
+void dtrsm_ (char *side, char *uplo, char *transa, char *diag, int *m, int *n, double *alpha,
+             double *a, int *lda, double *b, int *ldb);
+void dtrmm_ (char *side, char *uplo, char *transa, char *diag, int *m, int *n, double *alpha,
+             double *a, int *lda, double *b, int *ldb);
+void dsyrk_ (char *uplo, char *trans, int *n, int *k, double *alpha, double *a, int *lda,
+             double *beta, double *c, int *ldc);
+//void dpotrf_(const char* uplo, int& n, float* a, int* lda, int* info);
+}
+
+void potrf(double * const A, int ts, int ld)
+{
+   static int INFO;
+   static char L = 'L';
+   dpotrf_(&L, &ts, A, &ld, &INFO);
+}
+
+void trsm(double *A, double *B, int ts, int ld)
+{
+   static char LO = 'L', TR = 'T', NU = 'N', RI = 'R';
+   static double DONE = 1.0;
+   dtrsm_(&RI, &LO, &TR, &NU, &ts, &ts, &DONE, A, &ld, B, &ld );
+}
+
+void syrk(double *A, double *B, int ts, int ld)
+{
+   static char LO = 'L', NT = 'N';
+   static double DONE = 1.0, DMONE = -1.0;
+   dsyrk_(&LO, &NT, &ts, &ts, &DMONE, A, &ld, &DONE, B, &ld );
+}
+
+void gemm(double *A, double *B, double *C, int ts, int ld)
+{
+   static const char TR = 'T', NT = 'N';
+   static double DONE = 1.0, DMONE = -1.0;
+   dgemm_(&NT, &TR, &ts, &ts, &ts, &DMONE, A, &ld, B, &ld, &DONE, C, &ld);
+}
 
 static void fill_random(TiledMatrix &matrix);
 static void print_matrix(TiledMatrix &matrix);
+class LocalBlockCache;
+static
+void print_matrix(LocalBlockCache &block, size_t nx, size_t ny);
 
 class LocalBlockCache {
 public:
@@ -20,9 +70,8 @@ public:
     size_t block_col_idx)
   : _matrix(matrix) {
     auto& pattern       = matrix.pattern();
-    auto  blockspec     = pattern.blockspec();
-    auto  block_extents = blockspec.extents();
-    auto  glob_index    = block_row_idx*pattern.blocksize(0)*pattern.extent(1) + block_col_idx*pattern.blocksize(1);
+    auto  glob_index    = block_row_idx*pattern.blocksize(0)*pattern.extent(1)
+                            + block_col_idx*pattern.blocksize(1);
     this->_glob_idx     = glob_index;
     this->_is_local = pattern.is_local(glob_index);
     if (_is_local) {
@@ -31,30 +80,33 @@ public:
                       {block_row_idx*pattern.blocksize(0),
                         block_col_idx*pattern.blocksize(1)}).index;
     }
-    this->_size = block_extents[0] * block_extents[1];
+    this->_size = pattern.blocksize(0) * pattern.blocksize(1);
+#ifdef DEBUG
     std::cout << "BlockCache: " << block_row_idx << "x" << block_col_idx << " "
-              << block_extents[0] << "x" << block_extents[1]
               << " (size=" << _size << ", is_local=" << _is_local
               << ", glob_idx=" << glob_index
               << std::endl;
+#endif
   }
 
   ~LocalBlockCache() {
     if (!this->_is_local && _local_ptr != nullptr) {
+#ifdef DEBUG
       std::cout << "Freeing pointer " << _local_ptr << std::endl;
+#endif
       free(_local_ptr);
       _local_ptr = nullptr;
     }
   }
 
-  double *lbegin() {
+  value_t *lbegin() {
     if (!this->_is_local) {
       fetch_data();
     }
     return this->_local_ptr;
   }
 
-  double *lend() {
+  value_t *lend() {
     if (!this->_is_local) {
       fetch_data();
       return this->_local_ptr + this->_size;
@@ -70,63 +122,198 @@ public:
     return this->_size;
   }
 
+  void store() {
+    if (!_is_local && this->_local_ptr != NULL) {
+      auto begin = this->_matrix.begin() + _glob_idx;
+      dash::dart_storage<value_t> ds(_size);
+      dart_put_blocking(begin.dart_gptr(), _local_ptr, ds.nelem, ds.dtype);
+    }
+  }
+
 private:
 
   void fetch_data() {
     if (this->_local_ptr == nullptr) {
-      this->_local_ptr = static_cast<double*>(malloc(this->_size * sizeof(double)));
+      this->_local_ptr = static_cast<value_t*>(malloc(this->_size * sizeof(value_t)));
       auto begin = this->_matrix.begin() + _glob_idx;
-      auto end   = begin + _size;
-      std::cout << "Fetching " << dash::distance(begin, end) << " elements into "
+#ifdef DEBUG
+      std::cout << "Fetching " << _size << " elements into "
                 << _local_ptr << std::endl;
+#endif
+      dash::dart_storage<value_t> ds(_size);
       dart_get_blocking(
-        this->_local_ptr, begin.dart_gptr(),
-        dash::distance(begin, end), DART_TYPE_DOUBLE);
+        this->_local_ptr, begin.dart_gptr(), ds.nelem, ds.dtype);
       //dash::copy(begin, end, this->_local_ptr);
     }
   }
 
 private:
   TiledMatrix& _matrix;
-  double* _local_ptr = nullptr;
-  size_t  _size = 0;
-  size_t  _glob_idx;
-  bool    _is_local  = true;
+  value_t* _local_ptr = nullptr;
+  size_t   _size = 0;
+  size_t   _glob_idx;
+  bool     _is_local  = true;
 };
 
-static void
-dgemm_tiles(
-  TiledMatrix &matrix,
-  size_t num_blocks,
-  size_t block_size,
-  size_t row)
-{
-  for (size_t j = 0; j < row; ++j) {
-    LocalBlockCache block_b(matrix, row, j);
-    for (size_t k = row+1; k < num_blocks; ++k) {
-      LocalBlockCache block_c(matrix, k, row);
-      if (block_c.is_local()) {
-        LocalBlockCache block_a(matrix, k, j);
-        std::cout << "Calling cblas_dgemm on "
-            << block_a.lbegin() << " "
-            << block_b.lbegin() << " "
-            << block_c.lbegin() << std::endl;
-        cblas_dgemm(
-            CblasRowMajor, CblasNoTrans, CblasTrans,
-            block_size, block_size, block_size,
-            -1.0, block_a.lbegin(), block_size,
-                  block_b.lbegin(), block_size,
-             1.0, block_c.lbegin(), block_size);
+
+void
+compute_single(TiledMatrix& matrix, size_t block_size){
+
+  const size_t num_blocks = matrix.pattern().blockspec().extent(0);
+  /**
+   * Algorithm taken from
+   * https://pm.bsc.es/ompss-docs/examples/01-examples/cholesky
+   */
+
+  if (dash::myid() == 0){
+    // iterate over column of blocks
+    for (size_t k = 0; k < num_blocks; ++k) {
+
+      if (dash::myid() == 0)
+        std::cout << "Processing column " << k << std::endl;
+
+      LocalBlockCache block_k(matrix, k, k);
+
+      /**
+      * Compute cholesky factorization of block on diagonal
+      */
+      potrf(block_k.lbegin(), block_size, block_size);
+      block_k.store();
+
+      /**
+      * Solve the triangular equation system in the block
+      */
+      for (int i = k+1; i < num_blocks; ++i) {
+        LocalBlockCache block_b(matrix, k, i);
+        trsm(block_k.lbegin(),
+             block_b.lbegin(), block_size, block_size);
+        block_b.store();
+      }
+
+      // walk to the right
+      for (size_t i = k+1; i < num_blocks; ++i) {
+        // run down to the diagonal
+        LocalBlockCache block_a(matrix, k, i);
+        for (size_t j = k+1; j < i; ++j) {
+          LocalBlockCache block_c(matrix, j, i);
+          LocalBlockCache block_b(matrix, k, j);
+          // A[k,i] = A[k,i] - A[k,j] * (A[j,i])^t
+          gemm(block_a.lbegin(),
+               block_b.lbegin(),
+               block_c.lbegin(), block_size, block_size);
+          block_c.store();
+        }
+      }
+
+      // update diagonal blocks
+      for (size_t i = k+1; i < num_blocks; ++i) {
+        LocalBlockCache block_i(matrix, i, i);
+        LocalBlockCache block_a(matrix, k, i);
+        // A[j,j] = A[j,j] - A[j,i] * (A[j,i])^t
+        syrk(block_a.lbegin(),
+             block_i.lbegin(), block_size, block_size);
+        block_i.store();
       }
     }
   }
+
+  dash::barrier();
 }
 
+
+void
+compute(TiledMatrix& matrix, size_t block_size){
+
+  const size_t num_blocks = matrix.pattern().blockspec().extent(0);
+  /**
+   * Algorithm taken from
+   * https://pm.bsc.es/ompss-docs/examples/01-examples/cholesky
+   */
+
+  // iterate over column of blocks
+  for (size_t k = 0; k < num_blocks; ++k) {
+
+    if (dash::myid() == 0)
+      std::cout << "Processing column " << k << std::endl;
+
+    LocalBlockCache block_k(matrix, k, k);
+
+    /**
+     * Compute cholesky factorization of block on diagonal
+     */
+    if (block_k.is_local()) {
+      potrf(block_k.lbegin(), block_size, block_size);
+    }
+    dash::barrier();
+
+    /**
+     * Solve the triangular equation system in the block
+     */
+    for (int i = k+1; i < num_blocks; ++i) {
+      LocalBlockCache block_b(matrix, k, i);
+      if (block_b.is_local()) {
+        trsm(block_k.lbegin(),
+             block_b.lbegin(), block_size, block_size);
+      }
+    }
+
+    dash::barrier();
+
+    // walk to the right
+    for (size_t i = k+1; i < num_blocks; ++i) {
+      // run down to the diagonal
+      LocalBlockCache block_a(matrix, k, i);
+      for (size_t j = k+1; j < i; ++j) {
+        LocalBlockCache block_c(matrix, j, i);
+        if (block_c.is_local()) {
+          LocalBlockCache block_b(matrix, k, j);
+          // A[k,i] = A[k,i] - A[k,j] * (A[j,i])^t
+          gemm(block_a.lbegin(),
+               block_b.lbegin(),
+               block_c.lbegin(), block_size, block_size);
+        }
+      }
+    }
+    dash::barrier();
+
+    // update diagonal blocks
+    for (size_t i = k+1; i < num_blocks; ++i) {
+      LocalBlockCache block_i(matrix, i, i);
+      if (block_i.is_local()) {
+        LocalBlockCache block_a(matrix, k, i);
+        // A[j,j] = A[j,j] - A[j,i] * (A[j,i])^t
+        syrk(block_a.lbegin(),
+             block_i.lbegin(), block_size, block_size);
+      }
+    }
+    dash::barrier();
+  }
+
+}
 
 int main(int argc, char **argv)
 {
   dash::init(&argc, &argv);
-  const size_t  N = 10 * dash::size();
+  //const size_t  N = 15 * dash::size();
+#ifdef NUM_BLOCKS
+  static size_t block_size = N / NUM_BLOCKS;
+#elif defined(BLOCKS_PER_UNIT)
+  static size_t block_size = N / dash::size() / BLOCKS_PER_UNIT;
+#endif
+
+  /*
+  if (block_size * dash::size() * BLOCKS_PER_UNIT != N) {
+    if (dash::myid() == 0) {
+      std::cerr << "Invalid configuration detected: "
+                << "  BLOCKS_PER_UNIT: " << BLOCKS_PER_UNIT
+                << "  dash::size()   : " << dash::size()
+                << "  block_size     : " << block_size
+                << std::endl;
+    }
+    dart_abort(-1);
+    return -1;
+  }
+  */
 
   TiledMatrix matrix(N, N, dash::TILE(block_size), dash::TILE(block_size));
 
@@ -137,73 +324,50 @@ int main(int argc, char **argv)
   matrix.barrier();
   auto& pattern = matrix.pattern();
 
-  const size_t num_blocks = pattern.blockspec().extent(0);
 
   if (dash::myid() == 0) {
     std::cout << "block sizes: " << pattern.blocksize(0) << "x" << pattern.blocksize(1) << std::endl;
     std::cout << "num blocks: "  << pattern.blockspec().extent(0) << "x" << pattern.blockspec().extent(1) << std::endl;
   }
 
-  print_matrix(matrix);
-
-  // iterate over column of blocks
-  for (size_t i = 0; i < num_blocks; ++i) {
-
-    if (dash::myid() == 0)
-      std::cout << "Processing column " << i << std::endl;
-
-    dgemm_tiles(matrix, num_blocks, block_size, i);
-    dash::barrier();
-
-    for (size_t j = 0; j < i; ++j) {
-      LocalBlockCache block_b(matrix, i, i);
-      if (block_b.is_local()) {
-        LocalBlockCache block_a(matrix, i, j);
-        cblas_dsyrk(
-            CblasRowMajor, CblasLower, CblasNoTrans,
-            block_size, block_size,
-            -1.0, block_a.lbegin(), block_size,
-             1.0, block_b.lbegin(), block_size);
+#ifdef DEBUG
+  const size_t num_blocks = pattern.blockspec().extent(0);
+  if (N <= 20) {
+    print_matrix(matrix);
+    if (dash::myid() == 0) {
+      std::cout << "\n\nChecking tiles: " << std::endl;
+      for (size_t i = 0; i < num_blocks; ++i) {
+        for (size_t j = 0; j < num_blocks; ++j) {
+          std::cout << "Block " << i << "x" << j << ":\n";
+          LocalBlockCache block(matrix, i, j);
+          print_matrix(block, block_size, block_size);
+          std::cout << std::endl;
+        }
       }
     }
-    dash::barrier();
-
-    /**
-     * Compute cholesky factorization of block on diagonal
-     */
-//    std::array<typename TiledMatrix::index_type, 2> block_idx = {i, i};
-//    auto block = matrix.block(block_idx);
-    LocalBlockCache block(matrix, i, i);
-    if (block.is_local()) {
-      std::cout << "[" << dash::myid() << "] dpotrf on block ("
-                << i << ", " << i << ")"
-                << " [lbegin: " << block.lbegin() << "]"
-                << std::endl;
-      LAPACKE_dpotrf(LAPACK_ROW_MAJOR, 'L', block_size, block.lbegin(), block_size);
-    }
-
-    dash::barrier();
-    /**
-     * Solve the triangular equation system in the block
-     */
-    LocalBlockCache block_a(matrix, i, i);
-    for (int j = i+1; j < num_blocks; ++j) {
-//      std::array<typename TiledMatrix::index_type, 2> block_b_idx = {i, j};
-//      auto& block_b = matrix.block(block_b_idx);
-      LocalBlockCache block_b(matrix, i, j);
-      if (block_b.is_local()) {
-        cblas_dtrsm(
-          CblasRowMajor, CblasRight, CblasLower, CblasTrans, CblasNonUnit,
-          block_size, block_size,
-          1.0, block_a.lbegin(), block_size,
-               block_b.lbegin(), block_size);
-      }
-    }
-
-    dash::barrier();
   }
+#endif
 
-  print_matrix(matrix);
+#if defined(CHECK_RESULT) && N <= 20
+  TiledMatrix matrix_single(N, N, dash::TILE(block_size), dash::TILE(block_size));
+  // copy the matrix before compute
+  std::copy(matrix.lbegin(), matrix.lend(), matrix_single.lbegin());
+  dash::barrier();
+  if (dash::myid() == 0)
+    print_matrix(matrix_single);
+  // compute the correct answer on one unit
+  compute_single(matrix_single, block_size);
+  if (dash::myid() == 0) {
+    std::cout << "########## Expected Result ###############\n";
+    print_matrix(matrix_single);
+    std::cout << "##########################################\n";
+  }
+#endif
+
+  compute(matrix, block_size);
+
+  if (N <= 20)
+    print_matrix(matrix);
 
 
   dash::finalize();
@@ -214,11 +378,30 @@ int main(int argc, char **argv)
 static
 void fill_random(TiledMatrix &matrix)
 {
+#if defined(DEBUG) || defined(CHECK_RESULT)
+  // have unit 0 fill the whole matrix
+  //int c = 0;
+  if (dash::myid() == 0)
+  {
+    constexpr int rand_max = 100;
+    for (auto it = matrix.begin(); it != matrix.end(); ++it) {
+      *it = (rand())%(rand_max);
+      //*it = c++;
+    }
+  }
+#else
   constexpr int rand_max = 100;
+  for (auto it = matrix.lbegin(); it != matrix.lend(); ++it) {
+    *it = (rand())%(rand_max);
+  }
+#endif
+  // below causes invalid write!
+  /*
   dash::generate(
     matrix.begin(),
     matrix.end(),
     [&](){ return (rand())%(rand_max); });
+  */
 }
 
 static
@@ -226,15 +409,32 @@ void print_matrix(TiledMatrix &matrix)
 {
   if (matrix.extent(0) > 100) return;
 
-  std::cout << std::setw(5);
+  std::cout << std::setw(6);
 
   if (dash::myid() == 0) {
     for (size_t i = 0; i < matrix.extent(0); ++i) {
       for (size_t j = 0; j < matrix.extent(1); ++j) {
-        std::cout << std::setw(5) << static_cast<double>(matrix(i, j)) << " ";
+        std::cout << std::setw(5) << std::setprecision(3)
+                  << static_cast<value_t>(matrix(i, j)) << " ";
       }
       std::cout << std::endl;
     }
   }
 }
+
+
+static
+void print_matrix(LocalBlockCache &block, size_t nx, size_t ny)
+{
+  if (dash::myid() == 0) {
+    for (size_t i = 0; i < nx; ++i) {
+      for (size_t j = 0; j < ny; ++j) {
+        std::cout << std::setw(5) << std::setprecision(3)
+                  << static_cast<value_t>(*(block.lbegin() + i*ny+j)) << " ";
+      }
+      std::cout << std::endl;
+    }
+  }
+}
+
 
