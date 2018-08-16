36f255a9099999e393989c1e196816cf38cf03d5
Andreas Knüpfer <knue@posteo.de>
2018-08-16 20:26:02 +0200

    move writing of header line to the complie script, make sort inompile script work on Ubuntu where there seem to be subtle differences compared to the behavior of Debians sort

--- multigrid/combine_csvs.sh
+++ multigrid/combine_csvs.sh
@@ -4,8 +4,7 @@
 
 cat overview_0*.csv >"overview"$1".csv"
 rm -Rf overview_0*.csv
-`dirname $0`/overview.gnuplot
-# -e "filename='<name_of_tracefile.csv>'"
+`dirname $0`/overview.gnuplot -e "filename='<name_of_tracefile.csv>'"
 
 
 ls image_unit0.csv.* 2>/dev/null || exit 0
@@ -13,8 +12,8 @@ ls image_unit0.csv.* 2>/dev/null || exit 0
 for i in image_unit0.csv.*; do
     NUM=`echo $i|sed -e 's/image_unit0.csv.//'`
     echo -n $NUM" "
-    ##echo sort -n -m "image_unit"*".csv."$NUM ">" "image_"$1".csv."$NUM
-    sort -n -m "image_unit"*".csv."$NUM > "image_"$1".csv."$NUM
+    echo "z-index,y-index,x-index,z-coord,y-coord,x-coord,heat" > "image_"$1".csv."$NUM
+    sort "image_unit"*".csv."$NUM >> "image_"$1".csv."$NUM
     rm "image_unit"*".csv."$NUM
 done
 
