db4640680dbddf6189ff6cd8720a2ccbc8094701
Mietzsch <Nicco.Mietzsch@t-online.de>
2019-10-10 14:54:57 +0200

    changed compiler and deleted setparams

--- NPB/MG/npbparams.hpp
+++ NPB/MG/npbparams.hpp
@@ -1,23 +1,23 @@
-/* CLASS = W */
+/* CLASS = B */
 /*
 c  This file is generated automatically by the setparams utility.
 c  It sets the number of processors and the class_npb of the NPB
 c  in this directory. Do not modify it by hand.
 */
-#define	NX_DEFAULT	64
-#define	NY_DEFAULT	64
-#define	NZ_DEFAULT	64
-#define	NIT_DEFAULT	40
-#define	LM	6
-#define	LT_DEFAULT	6
+#define	NX_DEFAULT	256
+#define	NY_DEFAULT	256
+#define	NZ_DEFAULT	256
+#define	NIT_DEFAULT	20
+#define	LM	8
+#define	LT_DEFAULT	8
 #define	DEBUG_DEFAULT	0
-#define	NDIM1	6
-#define	NDIM2	6
-#define	NDIM3	6
+#define	NDIM1	8
+#define	NDIM2	8
+#define	NDIM3	8
 #define	CONVERTDOUBLE	FALSE
-#define COMPILETIME "02 Oct 2019"
+#define COMPILETIME "10 Oct 2019"
 #define NPBVERSION "4.0"
-#define CS1 "mpicxx"
+#define CS1 "dash-mpicxx"
 #define CS2 "$(CC)"
 #define CS3 "-L$(HOME)/opt/dash-0.4.0/lib"
 #define CS4 "-I../common -I$(HOME)/opt/dash-0.4.0/include"
